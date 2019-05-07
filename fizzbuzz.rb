b9e2845526deaf1786b266cd6434e5e461250688
Tracy Reuther <tracyreuther@gmail.com>
2019-05-07 22:28:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(n)
-  if n % 3
-    puts "Fizz"
-  elsif n % 5
-    puts "Buzz"
-  elsif n % 3 && n % 5
-    puts "FizzBuzz"
-  else n % 4
-    puts "nil"
+def fizzbuzz(number)
+  if (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"
+  elsif number % 5 == 0
+    "Buzz"
+  else number % 3 == 0
+    "Fizz"
 end
+
